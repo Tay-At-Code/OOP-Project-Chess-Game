9bf41b88c8bdc2298cae55eaa8271466155e5108
Tayyab <tayyabatta11@gmail.com>
2026-05-08 14:50:13 +0500

    String Member.h

--- Header.h
+++ Header.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <iostream>
 #include <string>
 using namespace std;
@@ -11,25 +12,29 @@ public:
     static bool isStraightClear(int fr, int fc, int tr, int tc, Piece* b[8][8]);
     static bool isDiagonalClear(int fr, int fc, int tr, int tc, Piece* b[8][8]);
 };
-class Piece {
+
+class Piece
+{
 protected:
     char color, symbol;
 public:
     Piece(char color, char symbol);
     virtual ~Piece() {}
-    char getColor()  const;
+    char getColor() const;
     char getSymbol() const;
+    virtual string toString() const;                                          
     virtual bool isValidMove(int fr, int fc, int tr, int tc, Piece* board[8][8]) = 0;
 };
 
-class Pawn : public Piece { public: Pawn(char c);   bool isValidMove(int, int, int, int, Piece* [8][8]); };
-class Rook : public Piece { public: Rook(char c);   bool isValidMove(int, int, int, int, Piece* [8][8]); };
-class Knight : public Piece { public: Knight(char c); bool isValidMove(int, int, int, int, Piece* [8][8]); };
-class Bishop : public Piece { public: Bishop(char c); bool isValidMove(int, int, int, int, Piece* [8][8]); };
-class Queen : public Piece { public: Queen(char c);  bool isValidMove(int, int, int, int, Piece* [8][8]); };
-class King : public Piece { public: King(char c);   bool isValidMove(int, int, int, int, Piece* [8][8]); };
+class Pawn : public Piece { public: Pawn(char c);   bool isValidMove(int, int, int, int, Piece* [8][8]); string toString() const; };
+class Rook : public Piece { public: Rook(char c);   bool isValidMove(int, int, int, int, Piece* [8][8]); string toString() const; };
+class Knight : public Piece { public: Knight(char c); bool isValidMove(int, int, int, int, Piece* [8][8]); string toString() const; };
+class Bishop : public Piece { public: Bishop(char c); bool isValidMove(int, int, int, int, Piece* [8][8]); string toString() const; };
+class Queen : public Piece { public: Queen(char c);  bool isValidMove(int, int, int, int, Piece* [8][8]); string toString() const; };
+class King : public Piece { public: King(char c);   bool isValidMove(int, int, int, int, Piece* [8][8]); string toString() const; };
 
-class Board {
+class Board
+{
     Piece* grid[8][8];
     char   turn;
     void   setup();
@@ -37,10 +42,10 @@ class Board {
 public:
     Board();
     ~Board();
-    bool   movePiece(int fr, int fc, int tr, int tc);
-    bool   isInCheck(char color);
-    bool   isCheckmate(char color);
-    bool   isStalemate(char color);
-    char   getTurn() const;
+    bool    movePiece(int fr, int fc, int tr, int tc);
+    bool    isInCheck(char color);
+    bool    isCheckmate(char color);
+    bool    isStalemate(char color);
+    char    getTurn() const;
     Piece* getPiece(int r, int c) const;
 };
